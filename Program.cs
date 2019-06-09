aa847f47a679dd0c4a0a1abe9d7543cf4b27d44b
Obelich <obelich@protonmail.com>
2019-06-09 14:37:58 -0700

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aprendiendo_NetcoreBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
